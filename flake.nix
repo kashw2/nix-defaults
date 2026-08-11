355b0ec576b332eb1f39eac6cafb4f6c95e98cb1
kashw2 <supra4keanu@hotmail.com>
2026-08-11 11:22:06 +1000

    feat(flake): add flake-parts

--- flake.nix
+++ flake.nix
@@ -3,8 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs: {
-  };
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
 }
