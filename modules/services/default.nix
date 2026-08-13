fb453e3e896b59468ed6d169b91d58723aa6e0d5
kashw2 <supra4keanu@hotmail.com>
2026-08-13 11:20:08 +1000

    fix(services): allow independent test configuration

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -1,20 +1,42 @@
 {
   inputs,
   config,
+  lib,
   ...
 }:
+let
+  processComposeModules = builtins.attrValues config.flake.processComposeModules;
+in
 {
   imports = [
     inputs.process-compose-flake.flakeModule
   ];
 
-  perSystem = ps: {
-    process-compose.default.imports = [
-      inputs.services-flake.processComposeModules.default
-      ./_test.nix
-    ]
-    ++ builtins.attrValues config.flake.processComposeModules;
+  perSystem =
+    { pkgs, config, ... }:
+    {
+      process-compose.default.imports = [
+        inputs.services-flake.processComposeModules.default
+        # This imoports tests and allows us to implement it as part of a working flake check
+        ./_test.nix
+        (
+          { config, ... }:
+          {
+            # Without setting the addr to the loopback it fails in the nix sandbox
+            services.pyroscope."telemetry:pyroscope".extraFlags = [
+              "-segment-writer.lifecycler.addr=${config.services.pyroscope."telemetry:pyroscope".httpAddress}"
+            ];
+          }
+        )
+      ]
+      ++ processComposeModules;
 
-    packages.test = ps.config.process-compose.default.outputs.testPackage;
-  };
+      packages.test = pkgs.writeShellApplication {
+        name = "test";
+        text = ''
+          export PC_DISABLE_TUI=1
+          exec ${lib.getExe config.process-compose.default.outputs.testPackage} "$@"
+        '';
+      };
+    };
 }
