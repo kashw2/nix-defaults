0027c39bc4cd511a1c200f513a1f137eb12386de
kashw2 <supra4keanu@hotmail.com>
2026-08-13 11:19:05 +1000

    fix(devShells): update lgtm to lgtp

--- modules/devShells/languages/golang.nix
+++ modules/devShells/languages/golang.nix
@@ -17,15 +17,15 @@ _: {
         env = {
           OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
           OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = "http://${
-            config.process-compose."default".services.tempo."lgtm:tempo".extraConfig.distributor.receivers.otlp.protocols.http.endpoint
+            config.process-compose."default".services.tempo."lgtp:tempo".extraConfig.distributor.receivers.otlp.protocols.http.endpoint
           }/v1/traces";
           OTEL_EXPORTER_OTLP_LOGS_ENDPOINT = "http://${
-            config.process-compose."default".services.loki."lgtm:loki".httpAddress
-          }:${toString config.process-compose."default".services.loki."lgtm:loki".httpPort}/otlp/v1/logs";
+            config.process-compose."default".services.loki."lgtp:loki".httpAddress
+          }:${toString config.process-compose."default".services.loki."lgtp:loki".httpPort}/otlp/v1/logs";
           OTEL_EXPORTER_OTLP_METRICS_ENDPOINT = "http://${
-            config.process-compose."default".services.prometheus."lgtm:prometheus".listenAddress
+            config.process-compose."default".services.prometheus."lgtp:prometheus".listenAddress
           }:${
-            toString config.process-compose."default".services.prometheus."lgtm:prometheus".port
+            toString config.process-compose."default".services.prometheus."lgtp:prometheus".port
           }/api/v1/otlp/v1/metrics";
         };
       };
