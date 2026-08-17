dd4dbc4e2ccaff4d716d17ad150f9d289e3a4752
kashw2 <supra4keanu@hotmail.com>
2026-08-17 12:48:07 +1000

    refactor(devshells): use mkOtelEnv helper

--- modules/devShells/languages/nodejs.nix
+++ modules/devShells/languages/nodejs.nix
@@ -1,8 +1,8 @@
-_: {
+{ self, ... }:
+{
   perSystem =
     {
       config,
-      lib,
       pkgs,
       self',
       ...
@@ -15,75 +15,7 @@ _: {
           pkgs.nodejs
         ];
 
-        env =
-          if (config.process-compose."default".services.alloy."telemetry:alloy".enable or false) then
-            {
-              OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
-              OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = "http://${
-                config.process-compose."default".services.alloy."telemetry:alloy".listenAddress
-              }:4328/v1/traces";
-              OTEL_EXPORTER_OTLP_LOGS_ENDPOINT = "http://${
-                config.process-compose."default".services.alloy."telemetry:alloy".listenAddress
-              }:4328/v1/logs";
-              OTEL_EXPORTER_OTLP_METRICS_ENDPOINT = "http://${
-                config.process-compose."default".services.alloy."telemetry:alloy".listenAddress
-              }:4328/v1/metrics";
-            }
-          else if (config.process-compose."default".services.openobserve."openobserve".enable or false) then
-            {
-              OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
-              OTEL_EXPORTER_OTLP_HEADERS = "Authorization=Basic YWRtaW5Ac2VydmljZXMtZmxha2UuY29tOkFkbWluMSFA";
-              OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = "http://${
-                config.process-compose."default".services.openobserve."openobserve".httpAddress
-              }:${
-                toString config.process-compose."default".services.openobserve."openobserve".httpPort
-              }/api/default/v1/traces";
-              OTEL_EXPORTER_OTLP_LOGS_ENDPOINT = "http://${
-                config.process-compose."default".services.openobserve."openobserve".httpAddress
-              }:${
-                toString config.process-compose."default".services.openobserve."openobserve".httpPort
-              }/api/default/v1/logs";
-              OTEL_EXPORTER_OTLP_METRICS_ENDPOINT = "http://${
-                config.process-compose."default".services.openobserve."openobserve".httpAddress
-              }:${
-                toString config.process-compose."default".services.openobserve."openobserve".httpPort
-              }/api/default/v1/metrics";
-            }
-          else
-            lib.optionalAttrs
-              (
-                (config.process-compose."default".services.tempo."lgtp:tempo".enable or false)
-                || (config.process-compose."default".services.loki."lgtp:loki".enable or false)
-                || (config.process-compose."default".services.prometheus."lgtp:prometheus".enable or false)
-              )
-              (
-                {
-                  OTEL_EXPORTER_OTLP_PROTOCOL = "http/protobuf";
-                  OTEL_EXPORTER_OTLP_HEADERS = "X-Scope-OrgID=1";
-                }
-                //
-                  lib.optionalAttrs (config.process-compose."default".services.tempo."lgtp:tempo".enable or false)
-                    {
-                      OTEL_EXPORTER_OTLP_TRACES_ENDPOINT = "http://${
-                        config.process-compose."default".services.tempo."lgtp:tempo".extraConfig.distributor.receivers.otlp.protocols.http.endpoint
-                      }/v1/traces";
-                    }
-                // lib.optionalAttrs (config.process-compose."default".services.loki."lgtp:loki".enable or false) {
-                  OTEL_EXPORTER_OTLP_LOGS_ENDPOINT = "http://${
-                    config.process-compose."default".services.loki."lgtp:loki".httpAddress
-                  }:${toString config.process-compose."default".services.loki."lgtp:loki".httpPort}/otlp/v1/logs";
-                }
-                //
-                  lib.optionalAttrs
-                    (config.process-compose."default".services.prometheus."lgtp:prometheus".enable or false)
-                    {
-                      OTEL_EXPORTER_OTLP_METRICS_ENDPOINT = "http://${
-                        config.process-compose."default".services.prometheus."lgtp:prometheus".listenAddress
-                      }:${
-                        toString config.process-compose."default".services.prometheus."lgtp:prometheus".port
-                      }/api/v1/otlp/v1/metrics";
-                    }
-              );
+        env = self.lib.mkOtelEnv pkgs.lib config.process-compose."default".services;
       };
     };
 }
