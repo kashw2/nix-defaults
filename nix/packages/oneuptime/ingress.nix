ef93b6d8dd990cd7970e9faf87afa32d5a78ee1e
kashw2 <supra4keanu@hotmail.com>
2026-09-02 16:33:23 +1000

    feat(oneuptime): package the nginx ingress configuration

---
+++ nix/packages/oneuptime/ingress.nix
@@ -0,0 +1,85 @@
+_: {
+  perSystem =
+    { pkgs, lib, ... }:
+    {
+      packages.oneuptime-ingress = pkgs.stdenv.mkDerivation (finalAttrs: {
+        pname = "oneuptime-ingress";
+        version = "12.0.28";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "OneUptime";
+          repo = "oneuptime";
+          tag = finalAttrs.version;
+          hash = "sha256-vOLlBXJL6/7fNgkDm5DYpfMtE2fEGywzts1m4Gtd5Jk=";
+        };
+
+        nativeBuildInputs = [
+          pkgs.gettext
+          pkgs.nginx
+        ];
+
+        passthru = {
+          port = 7849;
+          appPort = 3002;
+          dataDir = "./data/proxies:nginx";
+        };
+
+        buildCommand = ''
+          export HOST=127.0.0.1
+          export APP_PORT=${toString finalAttrs.passthru.appPort}
+          export HOME_PORT=${toString finalAttrs.passthru.appPort}
+          export SERVER_APP_HOSTNAME=127.0.0.1
+          export SERVER_HOME_HOSTNAME=127.0.0.1
+          export BACKEND_APP_TARGET='$backend_app'
+          export BACKEND_APP_GRPC_TARGET='$backend_app_grpc'
+          export BILLING_ENABLED=false
+          export NGINX_LISTEN_ADDRESS=127.0.0.1:
+          export NGINX_INGEST_ACCESS_LOG=on
+          export NGINX_RESOLVER=127.0.0.1
+          export SERVER_NAMES_HASH_BUCKET_SIZE=128
+          export SERVER_NAMES_HASH_MAX_SIZE=2048
+
+          sed '/# BEGIN upstream-keepalive/,/# END upstream-keepalive/d' \
+            ${finalAttrs.src}/Nginx/default.conf.template > template
+
+          envsubst "$(grep -oE '\$\{[A-Z_][A-Z0-9_]*\}' template | sort -u | tr '\n' ' ')" \
+            < template \
+            | sed \
+              -e 's#/var/log/nginx/#${finalAttrs.passthru.dataDir}/#g' \
+              -e 's#/etc/nginx/certs/#${finalAttrs.passthru.dataDir}/certs/#g' \
+              -e 's#^\([[:space:]]*listen[[:space:]][^;]*[^0-9]\)7849\([^0-9]\)#\1${toString finalAttrs.passthru.port}\2#' \
+            > servers.conf
+
+          ! grep -qE '\$\{[A-Z_]' servers.conf
+
+          sed -n '/^http {/,$p' ${finalAttrs.src}/Nginx/nginx.conf \
+            | sed \
+              -e '1d' \
+              -e '$d' \
+              -e '/include[[:space:]]*\/etc\/nginx\/mime\.types;/d' \
+              -e '/include[[:space:]]*\/etc\/nginx\/conf\.d\/default\.conf;/d' \
+              -e 's#/var/log/nginx/#${finalAttrs.passthru.dataDir}/#g' \
+            > http.conf
+
+          cat http.conf servers.conf > $out
+
+          mkdir -p ${finalAttrs.passthru.dataDir}/nginx ${finalAttrs.passthru.dataDir}/certs
+          {
+            echo 'pid nginx.pid;'
+            echo 'events { }'
+            echo 'http {'
+            cat $out
+            echo '}'
+          } > check.conf
+          nginx -t -p . -c check.conf -e stderr
+        '';
+
+        meta = {
+          description = "OneUptime nginx ingress configuration, for an nginx http block";
+          homepage = "https://oneuptime.com";
+          license = lib.licenses.asl20;
+          platforms = lib.platforms.unix;
+        };
+      });
+    };
+}
